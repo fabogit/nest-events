d0356a4a1f9d24f0c25f63b58a8cd523f15ad73d
fabogit <fabo.dev.email@gmail.com>
2023-06-17 18:42:08 +0200

    #8
    typeorm & entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { EventsController } from './events/events.controller';
+import { Event } from './events/event.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'nest-events',
+      entities: [Event],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
--- server/src/events/event.entity.ts
+++ server/src/events/event.entity.ts
@@ -1,7 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// @Entity('event', { name: 'event' })
+@Entity()
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column('varchar', { length: 100, nullable: false })
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   when: Date;
+
+  @Column()
   address: string;
 }
